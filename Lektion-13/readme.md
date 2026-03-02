9251c7e458b016edeaeeb943fa5e8f53c663102d
Michael Gustavsson <michael.gustavsson@softtech-dev.se>
2026-03-02 08:23:57 +0100

    Added starter project for lesson 13

---
+++ Lektion-13/readme.md
@@ -0,0 +1,3 @@
+# Lektion 13
+
+Idag ska vi fortsätta med att utforska TypeScript i vår Westcoast Cars applikation
---
+++ Lektion-13/westcoast-cars-starter/src/models/vehicle.ts
@@ -0,0 +1,10 @@
+export type Vehicle = {
+    id: number;
+    manufacturer: string;
+    model: string;
+    modelYear: number;
+    mileage: number;
+    value: number;
+    imageUrl: string;
+    description: string;
+}
