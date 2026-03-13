008a37e3b639dfb9867cc0ef1794358625496fb2
Michael Gustavsson <michael.gustavsson@softtech-dev.se>
2026-03-13 14:15:56 +0100

    Updated lesson-18

--- Lektion-18/westcoast-cars/src/config/env.ts
+++ Lektion-18/westcoast-cars/src/config/env.ts
@@ -1,4 +1,4 @@
 export const settings = {
-    BASE_URL: "https://westcoastcars-5865.restdb.io/rest/",
-    API_KEY: "69b15fe73e50057188050437"
+    BASE_URL: "<YOUR_API_URL>",
+    API_KEY: "<YOUR_API_KEY>"
 };
