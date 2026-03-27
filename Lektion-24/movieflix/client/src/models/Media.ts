7df49926ab8346d63f2414d373f7bb67a3f342e0
Michael Gustavsson <michael.gustavsson@softtech-dev.se>
2026-03-27 07:48:07 +0100

    Added working project and template html

---
+++ Lektion-24/movieflix/client/src/models/Media.ts
@@ -0,0 +1,26 @@
+import HttpClient from '../helpers/HttpClient';
+import { MediaResponse } from './media-response';
+
+export default class Media {
+    private _resource: string;
+    id: Number;
+    title: string;
+    language: string;
+    poster: string;
+    releaseDate: string;
+    rating: number;
+
+    constructor (resource: string) {
+        this._resource = resource;
+        this.id = 0;
+        this.title = "";
+        this.language = "";
+        this.poster = "";
+        this.releaseDate = "";
+        this.rating = 0.00;
+    }
+
+    async listMedia(): Promise<MediaResponse> {
+        return await new HttpClient<MediaResponse>(this._resource).listAll();
+    }
+}
